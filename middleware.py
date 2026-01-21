07618ebcbf5e144ef1aa9f7622eb000ad4393143
Ananya-source0510 <ananya.acharya0510@gmail.com>
2026-01-21 08:22:36 +0530

    Create middleware.py

---
+++ middleware.py
@@ -0,0 +1,10 @@
+import time
+from fastapi import Request
+
+@app.middleware("http")
+async def request_timing_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
